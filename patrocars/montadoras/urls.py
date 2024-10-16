c89a7e86fca50bdca63a58cb8a392cd94a706027
PauloRLeal <PauloRAraujoLeal@gmail.com>
2024-10-16 18:32:08 -0300

    [patrocars] feat: comecar update

--- patrocars/montadoras/urls.py
+++ patrocars/montadoras/urls.py
@@ -4,6 +4,7 @@ from .views import MontadoraView
 urlpatterns = [
     path('',MontadoraView.list_all),
     path('criar/',MontadoraView.create),
-    path('post/',MontadoraView.post,name='montadora_save')
-    
+    path('post/',MontadoraView.post,name='montadora_save'),
+    path('editar/<int:id>/',MontadoraView.edit),
+    path('put/',MontadoraView.put,name='montadora_edit'),
 ]
--- patrocars/montadoras/views.py
+++ patrocars/montadoras/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponseRedirect
 from .models import Montadora, MontadoraForm
 
 class MontadoraView:
     def list_all(req):
-        montadoras = Montadora.objects.all();
-        return render(req,'montadoras.html',context={'montadoras': montadoras});
+        montadoras = Montadora.objects.all()
+        return render(req,'montadoras/list_montadoras.html',context={'montadoras': montadoras})
 
     def create(req):
-        return render(req,'montadoras_form.html');
+        return render(req,'montadoras/create_montadora.html')
     
-    # def update(req):
-    #     return render(req,'montadoras_update_form.html');
+    def edit(req, id):
+        montadora = get_object_or_404(Montadora,id=id)
+        return render(req,'montadoras/update_montadora.html',context={'montadora': montadora})
     
-    # def put(req: HttpRequest):
-    #     form = MontadoraForm(req.POST)
-    #     if form.is_valid():
-    #         new_montadora =  Montadora(name=form.cleaned_data['name'],country=form.cleaned_data['country'] , foundation_year=form.cleaned_data['foundation_year'])    
-    #         new_montadora.save();
-    #     return render(req,'montadoras_form.html');
+    def put(req: HttpRequest):
+        form = MontadoraForm(req.POST)
+        if form.is_valid():
+            montadora = get_object_or_404(Montadora,id=form.cleaned_data['id'])
+            montadora.name = form.cleaned_data['name']
+            montadora.country = form.cleaned_data['country']
+            montadora.foundation_year = form.cleaned_data['foundation_year']
+            montadora.save()
+        return HttpResponseRedirect("/montadoras")
 
     def post(req: HttpRequest):
         form = MontadoraForm(req.POST)
+        #  if request.method == 'POST':
         if form.is_valid():
             new_montadora =  Montadora(name=form.cleaned_data['name'],country=form.cleaned_data['country'] , foundation_year=form.cleaned_data['foundation_year'])    
-            new_montadora.save();
-        return render(req,'montadoras_form.html');
+            new_montadora.save()
+        return HttpResponseRedirect("/montadoras")
